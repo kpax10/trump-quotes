60097bad6af6c527b7fccaa2ebe0a5aa7a9d54d8
Kyle <notxap55@yahoo.com>
2024-10-18 15:42:08 -0700

    starting app

--- main.py
+++ main.py
@@ -1,26 +1,39 @@
 from tkinter import *
+from PIL import Image, ImageTk
+import requests
 
 
 def get_quote():
-    pass
-    #Write your code here.
+    # url = 'https://api.tronalddump.io/random/quote'
+    url = 'https://api.whatdoestrumpthink.com/api/v1/quotes/random/'
+    response = requests.get(url)
+    data = response.json()
+    print(data)
 
+
+def resize_image(image, percentage):
+    width, height = image.size
+    new_width = int(width * (percentage / 100))
+    new_height = int(height * (percentage / 100))
+    return image.resize((new_width, new_height))
 
 
 window = Tk()
-window.title("Kanye Says...")
+window.title("Tronald Dump Says...")
 window.config(padx=50, pady=50)
 
 canvas = Canvas(width=300, height=414)
 background_img = PhotoImage(file="background.png")
 canvas.create_image(150, 207, image=background_img)
-quote_text = canvas.create_text(150, 207, text="Kanye Quote Goes HERE", width=250, font=("Arial", 30, "bold"), fill="white")
+quote_text = canvas.create_text(150, 207, text="Dump Quote Goes HERE", width=250, font=("Arial", 30, "bold"), fill="white")
 canvas.grid(row=0, column=0)
 
-kanye_img = PhotoImage(file="kanye.png")
-kanye_button = Button(image=kanye_img, highlightthickness=0, command=get_quote)
-kanye_button.grid(row=1, column=0)
-
+original_image = Image.open('trump-stupid.jpg')
+resize_percentage = 30  # Change this value as needed
+resized_image = resize_image(original_image, resize_percentage)
+photo = ImageTk.PhotoImage(resized_image)
+trump_button = Button(image=photo, highlightthickness=0, command=get_quote)
+trump_button.grid(row=1, column=0)
 
 
 window.mainloop()
